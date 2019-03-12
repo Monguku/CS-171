0e233f011e51b0ed20cb756d071ee20b815d2874
unknown <timthebananalord@gmail.com>
2019-03-12 16:52:51 -0400

    added HW8_17-2

---
+++ HW8_17-2.py
@@ -0,0 +1,37 @@
+def rev(text):
+    return text[::-1]
+
+if __name__ == "__main__":
+    print("Welcome to Digit Flipper")
+    userIn = str(input("Enter Some Text:\n"))
+    newString = ''
+    int_list = []
+    int_list.append([])
+    digits = "0123456789"
+    count = 0
+    for c in range(len(userIn)): #get 2d array of integers in the input
+        if c + 1  < len(userIn):
+            next_char = userIn[c + 1]
+            if userIn[c] in digits:
+                int_list[count].append(userIn[c])
+                if next_char not in digits:
+                    int_list.append([])
+                    count += 1
+
+        elif userIn[c] in digits:
+            int_list[count].append(userIn[c])
+
+
+    #print(int_list)
+    for i in range(len(int_list)):
+        for ii in range(len(int_list[i])):
+            newString += int_list[i][ii]
+        newString += '.'
+
+    int_list = newString.split('.')
+    int_list.pop()
+
+    for i in range(len(int_list)):
+        userIn = userIn.replace(int_list[i], rev(int_list[i]))
+    print("Revised String:")
+    print(userIn)
